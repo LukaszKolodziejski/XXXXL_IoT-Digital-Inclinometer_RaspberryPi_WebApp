299b71f46957fe8f30ec5ef1a1e77246811eb143
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-12-11 02:34:44 +0100

    clean code

--- raspberrypi/models/colors.js
+++
@@ -1,7 +0,0 @@
-export const O = [0, 0, 0];
-export const Xblue = [0, 75, 60];
-export const Xgreen = [60, 75, 0];
-export const Xorange = [85, 60, 0];
-export const Xpurple = [75, 0, 60];
-
-export const Xcolors = [Xblue, Xgreen, Xorange, Xpurple];
