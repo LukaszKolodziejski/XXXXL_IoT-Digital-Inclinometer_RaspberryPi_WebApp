3d79c3cb747c49cf8ccc24b4006b236860d8e4ab
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-12-11 02:40:12 +0100

    Working logic with IMU and Matrix of pixels

--- raspberrypi/app.js
+++ raspberrypi/app.js
@@ -2,16 +2,23 @@ import express from "express";
 
 import { Leds, Imu as imu } from "node-sense-hat";
 
-import * as Matrix from "./models/matrix.js";
+import * as Pixels from "./constants/pixels.js";
+import { O } from "./constants/colors";
+import { ValueX, AxisX, AxisY } from "./models/Matrix";
+
 import {
   setAxisRangePixel,
   separatedNumberX_,
   separatedNumber_X,
   setMainCross,
 } from "./controllers/matrix";
-import { O, Xcolors } from "./models/colors";
 
-const IMU = new imu.IMU();
+const IMU1 = new imu.IMU();
+const IMU2 = new imu.IMU();
+const IMU3 = new imu.IMU();
+const IMU4 = new imu.IMU();
+const IMU = [IMU1, IMU2, IMU3, IMU4];
+
 // const { Test } = require("./models/matrix");
 
 // const app = express();
@@ -34,29 +41,18 @@ const IMU = new imu.IMU();
 
 /////////<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-const valueX = {
-  angle: 64,
-  color: [Xcolors[0], Xcolors[1]],
-};
-const axisX = {
-  angle: 0,
-  color: Xcolors[3],
-};
-const axisY = {
-  angle: -60,
-  color: Xcolors[2],
-};
+Leds.setRotation(180);
 
+//TODO: fillOutMatirx to another folder
 const fillOutMatirx = (valueX, axisX, axisY) => {
-  Leds.setRotation(180);
-  const newMatrix = [...Matrix.emptyModel];
+  const newMatrix = [...Pixels.emptyModel];
   const separatedArrayX_ =
-    Matrix.modelsFrom0to9[separatedNumberX_(valueX.angle)];
+    Pixels.modelsFrom0to9[separatedNumberX_(valueX.angle)];
   const separatedArray_X =
-    Matrix.modelsFrom0to9[separatedNumber_X(valueX.angle)];
+    Pixels.modelsFrom0to9[separatedNumber_X(valueX.angle)];
 
-  const X_ = separatedArrayX_.map((px) => px + Matrix.startX_);
-  const _X = separatedArray_X.map((px) => px + Matrix.start_X);
+  const X_ = separatedArrayX_.map((px) => px + Pixels.startX_);
+  const _X = separatedArray_X.map((px) => px + Pixels.start_X);
 
   const axisXpx = setAxisRangePixel("X", axisX.angle);
   const axisYpx = setAxisRangePixel("Y", axisY.angle);
@@ -66,13 +62,16 @@ const fillOutMatirx = (valueX, axisX, axisY) => {
     .concat(axisYpx)
     .sort((a, b) => a - b);
 
+  const absX_ = valueX.angle >= 0 ? 1 : 0;
+  const abs_X = valueX.angle >= 0 ? 0 : 1;
+
   let pixel = 0;
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
       pixel = i * 8 + j;
       if (matrixArray.includes(pixel)) {
-        if (X_.includes(pixel)) newMatrix[i][j] = valueX.color[0];
-        if (_X.includes(pixel)) newMatrix[i][j] = valueX.color[1];
+        if (X_.includes(pixel)) newMatrix[i][j] = valueX.color[absX_];
+        if (_X.includes(pixel)) newMatrix[i][j] = valueX.color[abs_X];
         if (axisYpx.includes(pixel)) newMatrix[i][j] = axisY.color;
         if (axisXpx.includes(pixel)) newMatrix[i][j] = axisX.color;
       } else {
@@ -80,93 +79,100 @@ const fillOutMatirx = (valueX, axisX, axisY) => {
       }
     }
   }
-  Leds.setPixels(setMainCross(newMatrix));
+
+  Leds.sync.setPixels(setMainCross(newMatrix));
 };
 
-fillOutMatirx(valueX, axisX, axisY);
-
-// const intervalDelay = delay * matrixArray.length;
-// console.log(Leds);
-// setInterval(() => {
-//   for (let i = 0; i < matrixArray.length; i++)
-//     setTimeout(() => {
-//       const cross = matrixArray[i];
-//       const Xcolor = Xcolors[i];
-//       Leds.setPixels(setMainCross(cross(Xcolor)));
-//     }, delay * (i + 1));
-// }, intervalDelay);
+// !!!!!!!!!!!! MATRIX !!!!!!!!!!!!!
+// fillOutMatirx(valueX, axisX, axisY);
 
 // const axisY = [0, 8, 16, 24, 32, 40, 48, 56];
-
-// console.log(setAxisRangePixel("X", -90));
-// console.log(setAxisRangePixel("X", -60));
-// console.log(setAxisRangePixel("X", -40));
-// console.log(setAxisRangePixel("X", -20));
-// console.log(setAxisRangePixel("X", 0));
-// console.log(setAxisRangePixel("X", 20));
-// console.log(setAxisRangePixel("X", 40));
-// console.log(setAxisRangePixel("X", 60));
 // console.log(setAxisRangePixel("X", 80));
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-const pi = Math.PI;
-let ax = 0;
-let ay = 0;
-let az = 0;
-let x = 0;
-let y = 0;
-let z = 0;
-
-let angle_x = 0;
-let angle_y = 0;
+let actualSample = 0;
 let ax_sum = 0;
 let ay_sum = 0;
 let az_sum = 0;
 
-const angleArray = [];
+const angleArrayX = [];
+const angleArrayY = [];
+const timeArray = [];
 
+const loop = IMU.length;
 const countSample = 10; // aproximation 10
-const delay = 10; // 400
-const show = 20;
+const delay = 21; // 400
+const stack = 10;
 
 setInterval(() => {
-  for (let sample = 0; sample < countSample; sample++) {
-    //TODO: IMU nie w pętli for tylko, z opóźnieniem delay i od tego aproximation
-    IMU.getValue((err, data) => {
-      ax = data.accel["x"];
-      ay = data.accel["y"];
-      az = data.accel["z"];
-      ax_sum = ax_sum + ax;
-      ay_sum = ay_sum + ay;
-      az_sum = az_sum + az;
-    });
+  for (let i = 0; i < loop; i++) {
+    setTimeout(() => {
+      const start = new Date().getTime();
+      IMU[i].getValue((err, data) => {
+        const ax = data.accel["x"];
+        const ay = data.accel["y"];
+        const az = data.accel["z"];
+        ax_sum += ax;
+        ay_sum += ay;
+        az_sum += az;
+        const end = new Date().getTime();
+        const time = end - start;
+        timeArray.push(time);
+      });
+      actualSample++;
+    }, (delay / loop) * i);
   }
 
-  x = ax_sum / countSample;
-  y = ay_sum / countSample;
-  z = az_sum / countSample;
+  if (actualSample >= countSample) {
+    const x = ax_sum / countSample;
+    const y = ay_sum / countSample;
+    const z = az_sum / countSample;
 
-  // Calculate of roll and pitch in deg
-  angle_x = Math.atan2(x, Math.sqrt(y * y + z * z)) / (pi / 180);
-  angle_y = Math.atan2(y, Math.sqrt(x * x + z * z)) / (pi / 180);
+    // Calculate of roll and pitch in deg
+    const angle_x = Math.atan2(x, Math.sqrt(y * y + z * z)) / (Math.PI / 180);
+    const angle_y = Math.atan2(y, Math.sqrt(x * x + z * z)) / (Math.PI / 180);
 
-  // Reset values for next aproximation
-  ax_sum = 0;
-  ay_sum = 0;
-  az_sum = 0;
+    actualSample = 0;
+    ax_sum = ay_sum = az_sum = 0;
+    angleArrayX.push(angle_x.toFixed(0));
+    angleArrayY.push(angle_y.toFixed(0));
+  }
 
-  // console.log("angle_x");
-  // console.log(angle_x);
-  // console.log("angle_y");
-  // console.log(angle_y);
+  if (angleArrayX.length >= stack) {
+    // Reset values for next aproximation
 
-  angleArray.push(angle_x.toFixed(0));
-  if (angleArray.length === show) {
-    // const angle = angleArray.sort((a, b) => a - b)[show / 2];
-    const angle = angleArray.sort((a, b) => a - b);
-    console.log(`> > ${angle}`);
-    angleArray.length = 0;
-    console.log(Math.floor(delay / countSample));
+    const angleArrayXCopy = [...angleArrayX];
+    const angleArrayYCopy = [...angleArrayY];
+    angleArrayXCopy.sort((a, b) => a - b);
+    angleArrayYCopy.sort((a, b) => a - b);
+    console.log(`> > ${angleArrayXCopy}`);
+
+    const medianPointerArrayX = Math.floor(angleArrayXCopy.length / 2);
+    const medianPointerArrayY = Math.floor(angleArrayYCopy.length / 2);
+
+    const angleMedianArrayX = angleArrayXCopy[medianPointerArrayX];
+    const angleMedianArrayY = angleArrayYCopy[medianPointerArrayY];
+
+    console.log("angleMedianArrayX");
+    console.log(angleMedianArrayX);
+
+    ValueX.angle = angleMedianArrayX;
+    AxisX.angle = angleMedianArrayX;
+    AxisY.angle = angleMedianArrayY;
+
+    fillOutMatirx(ValueX, AxisX, AxisY);
+
+    angleArrayX.length = 0;
+    angleArrayY.length = 0;
+
+    let sumeOfTime = 0;
+    const loopDelay = delay / loop;
+    timeArray.forEach((time) => (sumeOfTime += time));
+    const sensorDelay = sumeOfTime / timeArray.length;
+
+    console.log(`loopDelay: ${loopDelay} | sensorDelay: ${sensorDelay}`);
+
+    timeArray.length = 0;
   }
 }, delay);
