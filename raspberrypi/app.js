010a7e85ef0f372d361994d5ed162760b4c07763
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2022-01-02 01:45:09 +0100

    working logic with MQTT & clean code with new folders

--- raspberrypi/app.js
+++ raspberrypi/app.js
@@ -1,144 +1,117 @@
-import express from "express";
-
 import { Leds } from "node-sense-hat";
 
 import { IMU } from "./constants/IMU";
 import { ValueX, AxisX, AxisY } from "./models/Matrix";
 import { fillOutMatirx } from "./controllers/matrix";
-import socket from "./socket";
-
-const app = express();
-const port = process.env.PORT || 8080;
-let httpRawData;
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  next();
-});
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use((err, req, res, next) => {
-  res.status(500).send({ message: err.message });
-});
-
-app.get("/", (req, res) => {
-  res.send("Serwer działa");
-  socket.getIO().emit("serverData", { action: "create", value: "dataSocket" });
-});
-
-app.get("/http-data", (req, res) => {
-  res.send({ value: httpRawData.value });
-});
-
-const server = app.listen(port, () => {
-  console.log(`Serve at http://localhost:${port}`);
-});
-
-const io = socket.init(server);
-io.on("connection", (socket) => {
-  console.log("Client connected");
-});
+import { initProtocols } from "./functions/initProtocols";
+import Socket from "./protocols/Socket";
+import Mqtt from "./protocols/Mqtt";
+import Http from "./protocols/Http";
+import { topic } from "./constants/mqtt";
 
 //TODO: Must be uncommented !!!
 // Leds.setRotation(180);
 
-let actualSample = 0;
-let ax_sum = 0;
-let ay_sum = 0;
-let az_sum = 0;
+const sendRawData = (rawData) => {
+  // HTTP
+  Http.setRawData(rawData);
+  // Websocket
+  Socket.getIO().emit("serverData", { action: "create", rawData });
+  // MQTT
+  Mqtt.getClient().publish(topic, rawData, { qos: 0, retain: false });
+};
 
-const angleArrayX = [];
-const angleArrayY = [];
-const timeArray = [];
+const main = () => {
+  let actualSample = 0;
+  let ax_sum = 0;
+  let ay_sum = 0;
+  let az_sum = 0;
 
-const loop = IMU.length;
-const countSample = 10; // aproximation 10
-const delay = 21;
-const stack = 10;
-let start, end, time;
+  const angleArrayX = [];
+  const angleArrayY = [];
+  const timeArray = [];
 
-setInterval(() => {
-  for (let i = 0; i < loop; i++) {
-    setTimeout(() => {
-      start = new Date().getTime();
-      IMU[i].getValue((err, data) => {
-        const ax = data.accel["x"];
-        const ay = data.accel["y"];
-        const az = data.accel["z"];
-        ax_sum += ax;
-        ay_sum += ay;
-        az_sum += az;
-        end = new Date().getTime();
-        time = end - start;
-        timeArray.push(time);
-      });
-      actualSample++;
-    }, (delay / loop) * i);
-  }
+  const loop = IMU.length;
+  const countSample = 10; // aproximation 10
+  const delay = 21;
+  const stack = 10;
+  let start, end, time;
 
-  if (actualSample >= countSample) {
-    const x = ax_sum / countSample;
-    const y = ay_sum / countSample;
-    const z = az_sum / countSample;
+  initProtocols();
 
-    // Calculate of roll and pitch in deg
-    const angle_x = Math.atan2(x, Math.sqrt(y * y + z * z)) / (Math.PI / 180);
-    const angle_y = Math.atan2(y, Math.sqrt(x * x + z * z)) / (Math.PI / 180);
+  setInterval(() => {
+    for (let i = 0; i < loop; i++) {
+      setTimeout(() => {
+        start = new Date().getTime();
+        IMU[i].getValue((err, data) => {
+          const ax = data.accel["x"];
+          const ay = data.accel["y"];
+          const az = data.accel["z"];
+          ax_sum += ax;
+          ay_sum += ay;
+          az_sum += az;
+          end = new Date().getTime();
+          time = end - start;
+          timeArray.push(time);
+        });
+        actualSample++;
+      }, (delay / loop) * i);
+    }
 
-    actualSample = 0;
-    ax_sum = ay_sum = az_sum = 0;
-    angleArrayX.push(angle_x.toFixed(0));
-    angleArrayY.push(angle_y.toFixed(0));
-  }
+    if (actualSample >= countSample) {
+      const x = ax_sum / countSample;
+      const y = ay_sum / countSample;
+      const z = az_sum / countSample;
 
-  if (angleArrayX.length >= stack) {
-    // Reset values for next aproximation
+      // Calculate of roll and pitch in deg
+      const angle_x = Math.atan2(x, Math.sqrt(y * y + z * z)) / (Math.PI / 180);
+      const angle_y = Math.atan2(y, Math.sqrt(x * x + z * z)) / (Math.PI / 180);
 
-    const angleArrayXCopy = [...angleArrayX];
-    const angleArrayYCopy = [...angleArrayY];
-    angleArrayXCopy.sort((a, b) => a - b);
-    angleArrayYCopy.sort((a, b) => a - b);
-    // console.log(`> > ${angleArrayXCopy}`);
+      actualSample = 0;
+      ax_sum = ay_sum = az_sum = 0;
+      angleArrayX.push(angle_x.toFixed(0));
+      angleArrayY.push(angle_y.toFixed(0));
+    }
 
-    const medianPointerArrayX = Math.floor(angleArrayXCopy.length / 2);
-    const medianPointerArrayY = Math.floor(angleArrayYCopy.length / 2);
+    if (angleArrayX.length >= stack) {
+      // Reset values for next aproximation
 
-    const angleMedianArrayX = angleArrayXCopy[medianPointerArrayX];
-    const angleMedianArrayY = angleArrayYCopy[medianPointerArrayY];
+      const angleArrayXCopy = [...angleArrayX];
+      const angleArrayYCopy = [...angleArrayY];
+      angleArrayXCopy.sort((a, b) => a - b);
+      angleArrayYCopy.sort((a, b) => a - b);
+      // console.log(`> > ${angleArrayXCopy}`);
 
-    // console.log("angleMedianArrayX");
-    // console.log(angleMedianArrayX);
+      const medianPointerArrayX = Math.floor(angleArrayXCopy.length / 2);
+      const medianPointerArrayY = Math.floor(angleArrayYCopy.length / 2);
 
-    ValueX.angle = angleMedianArrayX;
-    AxisX.angle = angleMedianArrayX;
-    AxisY.angle = angleMedianArrayY;
+      const angleMedianArrayX = angleArrayXCopy[medianPointerArrayX];
+      const angleMedianArrayY = angleArrayYCopy[medianPointerArrayY];
 
-    // Websocket
-    socket
-      .getIO()
-      .emit("serverData", { action: "create", value: ValueX.angle });
+      // console.log("angleMedianArrayX");
+      // console.log(angleMedianArrayX);
 
-    // Http
-    httpRawData = { value: ValueX.angle };
+      ValueX.angle = angleMedianArrayX;
+      AxisX.angle = angleMedianArrayX;
+      AxisY.angle = angleMedianArrayY;
 
-    fillOutMatirx(ValueX, AxisX, AxisY);
+      sendRawData(ValueX.angle);
 
-    angleArrayX.length = 0;
-    angleArrayY.length = 0;
+      fillOutMatirx(ValueX, AxisX, AxisY);
 
-    let sumeOfTime = 0;
-    const loopDelay = delay / loop;
-    timeArray.forEach((time) => (sumeOfTime += time));
-    const sensorDelay = sumeOfTime / timeArray.length;
+      angleArrayX.length = 0;
+      angleArrayY.length = 0;
 
-    // console.log(`loopDelay: ${loopDelay} | sensorDelay: ${sensorDelay}`);
+      let sumeOfTime = 0;
+      const loopDelay = delay / loop;
+      timeArray.forEach((time) => (sumeOfTime += time));
+      const sensorDelay = sumeOfTime / timeArray.length;
 
-    timeArray.length = 0;
-  }
-}, delay);
+      // console.log(`loopDelay: ${loopDelay} | sensorDelay: ${sensorDelay}`);
+
+      timeArray.length = 0;
+    }
+  }, delay);
+};
+
+main();
