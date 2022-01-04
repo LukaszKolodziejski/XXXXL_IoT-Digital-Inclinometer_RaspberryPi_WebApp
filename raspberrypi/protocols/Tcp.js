c840fcb27820cdb59f9ce9e7a73bd2883de08551
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2022-01-04 01:43:23 +0100

    tcp server

---
+++ raspberrypi/protocols/Tcp.js
@@ -0,0 +1,53 @@
+import Net from "net";
+import Server from "./Http";
+
+let serverSocketTcp;
+
+export default {
+  init: (port) => {
+    // Server TCP
+    let streamTcp;
+
+    const portTCP = port;
+    const hostTCP = "192.168.1.131";
+
+    var serverTCP = Net.createServer((socket) => {
+      serverSocketTcp = socket;
+      socket.write("1 Echo serverTCP\r\n");
+      socket.pipe(socket);
+    });
+
+    serverTCP.on("listening", () => {
+      console.log("server TCP is running");
+    });
+
+    serverTCP.on("connection", (stream) => {
+      const server = Server.init(1338);
+      streamTcp = require("socket.io")(server);
+      stream.write("stream Echo serverTCP");
+    });
+
+    serverTCP.listen(portTCP, hostTCP);
+
+    // Client TCP
+
+    var clientTCP = new Net.Socket();
+    clientTCP.connect(portTCP, hostTCP, () => {
+      clientTCP.write("Client TCP is Connected");
+    });
+
+    clientTCP.on("data", (rawData) => {
+      console.log("Received: " + rawData);
+      streamTcp.emit("serverDataTCP", {
+        action: "create",
+        rawData: `${rawData}`,
+      });
+    });
+
+    return serverSocketTcp;
+  },
+  getSocket: () => {
+    if (!serverSocketTcp) throw new Error("Server Socket Tcp not initialized!");
+    return serverSocketTcp;
+  },
+};
