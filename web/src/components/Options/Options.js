e7fe70652e53ae03619463dcbb5493a419518ca3
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2022-01-14 03:13:06 +0100

    Options to Orientation

--- web/src/components/Options/Options.js
+++ web/src/components/Options/Options.js
@@ -1,33 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styles from "./Options.module.css";
 import OptionsItem from "./OptionsItem/OptionsItem";
 
 const Options = (props) => {
-  const [options, setOptions] = useState(
-    props.values.map((value) => ({ text: value, active: false }))
-  );
-
-  const clickOptionItemHandler = (option, active) => {
-    const newOptions = options.map((opt) =>
-      opt.text === option ? { ...opt, active } : opt
-    );
-    setOptions(newOptions);
-  };
-
-  useEffect(() => {
-    console.log(options);
-  }, [options]);
-
   return (
     <div className={styles.Options}>
       {props.values.map((value) => {
-        return (
-          <OptionsItem
-            key={value}
-            text={value}
-            onValue={clickOptionItemHandler}
-          />
-        );
+        return <OptionsItem key={value} text={value} onValue={props.onClick} />;
       })}
     </div>
   );
