e7c7070b7888900c1283b8a0660ffedff6507bac
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2022-01-14 03:21:32 +0100

    working Chart with dummy data in Orintation.js

--- web/src/containers/Orientation/Orientation.js
+++ web/src/containers/Orientation/Orientation.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Options from "../../components/Options/Options";
+import Chart from "../../components/Chart/Chart";
 import styles from "./Orientation.module.css";
 
+const NAMES = ["X", "Y", "Z"];
+
 const Orientation = (props) => {
+  const [options, setOptions] = useState(
+    NAMES.map((name) => ({ text: name, active: false }))
+  );
+
+  const clickOptionItemHandler = (option, active) => {
+    const newOptions = options.map((opt) =>
+      opt.text === option ? { ...opt, active } : opt
+    );
+    setOptions(newOptions);
+  };
+
+  useEffect(() => {
+    console.log(options);
+  }, [options]);
+
   return (
     <div className={styles.Orientation}>
-      <Options values={["X", "Y", "Z"]} />
-      <div>Wykres</div>
+      <Options values={NAMES} onClick={clickOptionItemHandler} />
+      <Chart options={options} />
       <div>Suwak</div>
     </div>
   );
