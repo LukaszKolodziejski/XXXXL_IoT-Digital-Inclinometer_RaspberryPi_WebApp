a48e9afff2a545d9fa1f0e1a7629faae7ca9ed95
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2022-01-14 03:19:39 +0100

    Button

---
+++ web/src/components/UI/Button/Button.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+const Button = (props) => (
+  <button
+    className={[styles.Button, styles[props.btnType]].join(" ")}
+    disabled={props.disabled}
+    onClick={props.clicked}
+  >
+    {props.children}
+  </button>
+);
+
+export default Button;
