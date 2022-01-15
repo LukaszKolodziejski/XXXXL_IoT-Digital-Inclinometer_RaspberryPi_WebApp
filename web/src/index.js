800abafc40d051fbc8694ad6e663e1b2f0036962
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2022-01-16 00:45:50 +0100

    redux

--- web/src/index.js
+++ web/src/index.js
@@ -3,10 +3,28 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import orientationReducer from "./store/reducers/orientation";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = {
+  orientation: orientationReducer,
+};
+
+const store = createStore(
+  combineReducers(rootReducer),
+  composeEnhancers(applyMiddleware(thunk))
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
 
