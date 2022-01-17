7035fac57c493ffff4bf6ad305d4c59c99172580
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2022-01-17 05:37:39 +0100

    move logic to redux

--- web/src/containers/Orientation/Orientation.js
+++ web/src/containers/Orientation/Orientation.js
@@ -1,46 +1,32 @@
 import React, { useState, useEffect, useCallback, useRef } from "react";
-import openSocket from "socket.io-client";
 
 import Options from "../../components/Options/Options";
 import Chart from "../../components/Chart/Chart";
 import styles from "./Orientation.module.css";
-import {
-  CLIENT_ID,
-  CONNECT_URL,
-  TOPIC,
-  OPTIONS,
-} from "../../constants/protocols";
 import { useDispatch, useSelector } from "react-redux";
-
+import { countAnglesHandler } from "../../functions/functions";
 import * as actions from "../../store/actions/index";
 
 const NAMES = ["X", "Y", "Z"];
-
-// >>>>>>>>>>>>>>>>>>>>>>>> New
-// const initData = {
-//   data: null,
-//   transferTime: 0,
-//   angle: {
-//     X: 0,
-//     Y: 0,
-//     Z: 0,
-//   },
-// };
-// <<<<<<<<<<<<<<<<<<<<<<<<
 
 const Orientation = React.memo((props) => {
   const [options, setOptions] = useState(
     NAMES.map((name) => ({ text: name, active: false }))
   );
-  const [syncTime, setSyncTime] = useState(0);
-  const syncTimeRef = useRef();
+
+  const websocketRawData = useSelector(
+    (state) => state.protocols.websocket.rawData
+  );
+  const syncTime = useSelector((state) => state.protocols.websocket.syncTime);
 
   const dispatch = useDispatch();
   const onSocketValue = (ws) => dispatch(actions.socketValueHandler(ws));
 
+  const activeWebsocketRef = useRef();
+
   useEffect(() => {
-    syncTimeRef.current = syncTime;
-  });
+    activeWebsocketRef.current = options[0].active;
+  }, [options]);
 
   const clickOptionItemHandler = (option, active) => {
     const newOptions = options.map((opt) =>
@@ -49,70 +35,35 @@ const Orientation = React.memo((props) => {
     setOptions(newOptions);
   };
 
-  useEffect(() => {
-    console.log(options);
-  }, [options]);
-
   const receivedDataHandler = useCallback(
     (proof) => {
       if (proof.rawData) {
         const { data, startTime } = proof.rawData;
         const endTime = new Date().getTime();
-        const transferTime = endTime - startTime - syncTimeRef.current;
+        const transferTime = endTime - startTime - syncTime;
+        const angle = countAnglesHandler(data);
         // console.log("transferTime");
         // console.log(transferTime);
 
-        const x = data.accel["x"];
-        const y = data.accel["y"];
-        const z = data.accel["z"];
-
-        const angle_x =
-          Math.atan2(x, Math.sqrt(y * y + z * z)) / (Math.PI / 180);
-        const angle_y =
-          Math.atan2(y, Math.sqrt(x * x + z * z)) / (Math.PI / 180);
-        const angle_z =
-          Math.atan2(z, Math.sqrt(x * x + y * y)) / (Math.PI / 180);
-
-        const angle = {
-          X: angle_x,
-          Y: angle_y,
-          Z: angle_z,
-        };
-
         return {
-          data,
           angle,
           transferTime,
         };
       }
     },
-    [syncTimeRef]
+    [syncTime]
   );
 
-  const timeSynchronizationHandler = (socket) => {
-    socket.on("syncTime", (data) => {
-      if (data.action === "sync") {
-        const end = new Date().getTime();
-        setSyncTime(end - data.start);
-      }
-    });
-  };
-
+  // Websocket
   useEffect(() => {
-    // Websocket
-    const socket = openSocket("http://192.168.1.131:8080/", OPTIONS);
-    timeSynchronizationHandler(socket);
-    socket.on("serverData", (data) => {
-      // if (data.action === "create") setSocketValue(receivedDataHandler(data));
-      if (data.action === "create") onSocketValue(receivedDataHandler(data));
-    });
-  }, []);
+    if (activeWebsocketRef.current)
+      onSocketValue(receivedDataHandler(websocketRawData));
+  }, [websocketRawData]);
 
   return (
     <div className={styles.Orientation}>
       <Options text="Axis" values={NAMES} onClick={clickOptionItemHandler} />
       <Chart kind="orientation" options={options} />
-      {/* <div></div> Suwak */}
     </div>
   );
 });
