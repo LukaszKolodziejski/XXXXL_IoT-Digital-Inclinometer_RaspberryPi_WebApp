58574917978899b9f00a32a19da01b3d05a2f812
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2022-01-17 18:43:07 +0100

    fix Orientation bug in axis

--- web/src/constants/colors.js
+++ web/src/constants/colors.js
@@ -1,12 +1,12 @@
 const COLORS = {
-  X: "#00d0ff",
-  Y: "#95ff00",
-  Z: "#ffc800",
+  X: "#77cc00",
+  Y: "#00afd6",
+  Z: "#cfcf02",
 
-  WebSocket: "#00d0ff",
-  HTTP: "#95ff00",
+  WebSocket: "#77cc00",
+  HTTP: "#00afd6",
   MQTT: "#ff0800",
-  TCP: "#ebeb05",
+  TCP: "#cfcf02",
   UDP: "#8800ff",
 
   option: "rgb(211, 37, 37)",
