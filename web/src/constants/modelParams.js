76bc99ada82092a0cb4f33a1d1d6e4f0897d1451
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2022-01-19 03:43:12 +0100

    constants

---
+++ web/src/constants/modelParams.js
@@ -0,0 +1,14 @@
+const modelParams = {
+  flipHorizontal: true,
+  outputStride: 16,
+  imageScaleFactor: 1,
+  maxNumBoxes: 20,
+  iouThreshold: 0.2,
+  scoreThreshold: 0.6,
+  modelType: "ssd320fpnlite",
+  modelSize: "large",
+  bboxLineWidth: "2",
+  fontSize: 17,
+};
+
+export { modelParams };
