a249694bafe114d1803bc5ed06b32b7ab3cee1d4
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2022-02-16 23:45:02 +0100

    optimize code app.js

--- raspberrypi/app.js
+++ raspberrypi/app.js
@@ -19,7 +19,6 @@ const rawDataHandler = (data) => {
 };
 
 const sendRawData = (data) => {
-  // console.log(rawDataHandler(data));
   // HTTP
   Http.setRawData(rawDataHandler(data));
 
@@ -86,41 +85,12 @@ const main = () => {
       const loopDelay = delay / loop;
       timeArray.forEach((time) => (sumeOfTime += time));
       const sensorDelay = sumeOfTime / timeArray.length;
-      // console.log(timeArray);
-      // console.log(
-      //   `loopDelay: ${loopDelay} ms | sensorDelay: ${sensorDelay} ms`
-      // );
+      // console.log(`loopDelay: ${loopDelay} ms | sensorDelay: ${sensorDelay} ms`);
       timeSample = 0;
       timeArray.length = 0;
     }
   };
 
-  function y(x) {
-    // Declaring the function
-    // f(x) = 1/(1+x*x)
-    return 1 / (1 + x * x);
-  }
-
-  // Function to evaluate the value of integral
-  function trapezoidal(a, b, n) {
-    // Grid spacing
-    let h = (b - a) / n;
-
-    // Computing sum of first and last terms
-    // in above formula
-    let s = y(a) + y(b);
-
-    // Adding middle terms in above formula
-    for (let i = 1; i < n; i += 1) s += 2 * y(a + i * h);
-
-    // h/2 indicates (b-a)/2n. Multiplying h/2
-    // with s.
-    return (h / 2) * s;
-  }
-
-  let angleX = 0;
-  let previousGyroAngleX = 0;
-  let angle = 0;
   setInterval(() => {
     timeSynchronization();
     for (let i = 0; i < loop; i++) {
@@ -130,43 +100,15 @@ const main = () => {
           end = new Date().getTime();
           sendRawData(data);
           timeHandler(start);
-          // console.log(rawDataHandler(data));
 
           const x = data.accel["x"];
           const y = data.accel["y"];
           const z = data.accel["z"];
-          const gx = data.gyro["x"];
-          const gy = data.gyro["y"];
-          const gz = data.gyro["z"];
 
           const angle_x =
             Math.atan2(x, Math.sqrt(y * y + z * z)) / (Math.PI / 180);
           const angle_y =
             Math.atan2(y, Math.sqrt(x * x + z * z)) / (Math.PI / 180);
-
-          // Imitacja !!
-          const accAngleX = Math.atan2(x, Math.sqrt(y * y + z * z));
-          const accAngleY = Math.atan2(y, Math.sqrt(x * x + z * z));
-
-          const dt = end - start;
-          const f = 200; // Hz
-          const T = 1 / (2 * Math.PI * f);
-
-          const K = T / (T + dt);
-
-          const gyroAngleX = trapezoidal(0, gx, dt);
-
-          if (angleX === 0) {
-            angleX = K * (gyroAngleX * dt) + (1 - K) * accAngleX;
-          } else {
-            angleX =
-              K * (angleX + gyroAngleX - previousGyroAngleX) +
-              (1 - K) * accAngleX;
-          }
-          previousGyroAngleX = gyroAngleX;
-
-          // angle = Math.floor(angleX * 58);
-          // console.log(angle);
 
           xSamplesArray = samplesHandler(angle_x, xSamplesArray);
           ySamplesArray = samplesHandler(angle_y, ySamplesArray);
